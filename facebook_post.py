34f4c266d3e19793e87e7fd596aedb1288d5d3ce
Ljuba Nedeljkovic <ljuba.ned@gmail.com>
2012-12-06 12:59:34 +0100

    Removing unnecessary files.

--- facebook_post.py
+++
@@ -1,8 +0,0 @@
-
-class FacebookPost:
-    self.poster = 'ljuba'
-    self.avatar = 'http://facebook.com/ljuba.nedeljkovic.7/picture'
-    self.subject = 'Dummy subject'
-    self.text = 'Dummy text'
-    self.image = 'http://url.to.image.com/image.jpg'
-    self.post_date = '2012-12-9 11:25AM'
--- facebook_posts.py
+++
@@ -1,5 +0,0 @@
-
-class FacebookPosts:
-    self.posts = []
-    self.previous_url = ''
-    self.next_url = ''

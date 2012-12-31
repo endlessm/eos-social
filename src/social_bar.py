7222dcbfb2435d4356bf891b1f45544467c59ce5
Bojan Radojevic <bradojevic@gmail.com>
2012-12-31 16:08:04 +0100

    Added basic single instance checking and control.

--- src/social_bar.py
+++ src/social_bar.py
@@ -2,10 +2,19 @@
 from social_bar_view import SocialBarView
 from social_bar_presenter import SocialBarPresenter
 from social_bar_model import SocialBarModel
+from util.single_instance import DBusSingleAppInstance
 
 
 if __name__ == "__main__":
-    presenter = SocialBarPresenter(SocialBarView(), SocialBarModel())
-    presenter.get_view().set_presenter(presenter)
-    presenter.get_view().main()
+    running = False
+    try:
+        running = DBusSingleAppInstance.is_running()
+    except:
+        pass
+    if running:
+        print "Social Bar already running!"
+    else:
+        presenter = SocialBarPresenter(SocialBarView(), SocialBarModel())
+        presenter.get_view().set_presenter(presenter)
+        presenter.get_view().main()
 

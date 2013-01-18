fd778221d7648d8f973f4aaba1ee3ffce774bbca
Ljuba Nedeljkovic <ljuba.ned@gmail.com>
2013-01-18 16:58:20 +0100

    Using .ini file values instead of hardcoded ones.

--- src/defaults.py
+++ src/defaults.py
@@ -5,3 +5,4 @@ class Defaults():
     FB_APP_ID = '407909575958642'
     FB_APP_SECRET = '496f85b88366ae40b42d16579719815c'
     FB_LOGIN_MODULE_PATH='/usr/share/eos-social/facebook/fb_auth_window.pyc'
+    FB_AUTO_REFRESH_INTERVAL = 60000

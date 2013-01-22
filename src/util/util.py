40d3fb0edb8e9a645e0480c62e1d13bb64c60d47
Ljuba Nedeljkovic <ljuba@Ljuba-Mint.(none)>
2013-01-22 12:51:32 +0100

    Getting comments by FQL instead of standard graph API call. Removed js function that is not needed and changed post comment form from dynamic to static.

--- src/util/util.py
+++ src/util/util.py
@@ -1543,3 +1543,17 @@ WHERE filter_key IN
 ORDER BY created_time DESC
 LIMIT 25
 '''
+
+comments_query = '''
+SELECT id, fromid, text, can_remove, user_likes, time, likes
+FROM comment
+WHERE object_id = 483083671730084
+ORDER BY time DESC
+LIMIT 10
+'''
+
+comments_users_query = '''
+SELECT uid, first_name, last_name, name
+FROM user
+WHERE uid IN (SELECT fromid FROM #comments)
+'''

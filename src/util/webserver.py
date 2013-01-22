e19717d753918c255da25b25faa30180a09c60e4
Ljuba Nedeljkovic <ljuba@Ljuba-Mint.(none)>
2013-01-22 17:47:48 +0100

    Replaced presenter.navigator() call with asynchronous javascript.

--- src/util/webserver.py
+++ src/util/webserver.py
@@ -1,6 +1,6 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from facebook.facebook import GraphAPIError, GraphAPI
-from util import posts_query, users_query, older_posts_query, newer_posts_query
+from util import posts_query, users_query, older_posts_query, newer_posts_query, comments_query, comments_users_query
 from facebook.facebook_posts import FacebookPosts
 from social_bar_model import SocialBarModel
 import json
@@ -17,31 +17,51 @@ class MyHandler(BaseHTTPRequestHandler):
     
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
-
         parsed_qry = urlparse.parse_qs(parsed_path.query)
         jsonp_string = parsed_qry['onJSONPLoad'][0]
-        timestamp = parsed_qry['timestamp'][0]
         
+        print '-'*80
+        print 'PARSED PATH :', parsed_path
+        print 'PARSED QUERY:', parsed_qry
+        print 'JSONP STRING:', jsonp_string
+        print '-'*80
         if 'newer' in parsed_path.path.lower():
+            timestamp = parsed_qry['timestamp'][0]
             fb_response = self.get_new_fb_posts(timestamp, False)
         elif 'older' in parsed_path.path.lower():
+            timestamp = parsed_qry['timestamp'][0]
             fb_response = self.get_new_fb_posts(timestamp, True)
+        elif 'comment' in parsed_path.path.lower():
+            postid = parsed_qry['id'][0]
+            fb_response = self.get_comments(postid)
         else:
             fb_response = None
         
         if not fb_response:
             posts = {}
         else:
-            posts = self.parse_posts(fb_response)
+            if 'comment' not in parsed_path.path.lower():
+                posts = self.parse_posts(fb_response)
         
-        html = self.generate_posts_elements(posts.posts)
         data = {}
-        data['html'] = str(html)
+        
         if 'older' in parsed_path.path.lower():
+            html = self.generate_posts_elements(posts.posts)
+            data['html'] = str(html)
             data['next_url'] = posts.next_url
-        else:
+        elif 'newer' in parsed_path.path.lower():
+            html = self.generate_posts_elements(posts.posts)
+            data['html'] = str(html)
             data['previous_url'] = posts.previous_url
-            
+        elif 'comment' in parsed_path.path.lower():
+#            data = fb_response
+            data['html'] = str(self.generate_comments(fb_response, parsed_qry['id'][0]))
+            print '+'*80
+            print data['html']
+            print '+'*80
+            print '+'*80
+            print simplejson.dumps(data['html'])
+            print '+'*80
         to_write = jsonp_string + '(' + simplejson.dumps(data) + ')'
         
         self.send_response(200)
@@ -92,6 +112,63 @@ class MyHandler(BaseHTTPRequestHandler):
                   {'auto_refresh_interval':Settings.FB_AUTO_REFRESH_INTERVAL}]
         page = Template(file = '/usr/share/eos-social/templates/posts-array.html', searchList = params)
         return page
+    
+    def get_comments(self, post_id, until=0):
+        query = {'comments':comments_query,'users':comments_users_query}
+        try:
+            _model = SocialBarModel()
+            _token = _model.get_stored_fb_access_token()
+            _graph_api = GraphAPI(access_token=_token)
+            result = _graph_api.fql(query)
+#            print 'In get_comments...'
+#            print '-'*80
+#            pprint.pprint(result)
+#            print '-'*80
+            comments = self.parse_comments(result)
+        except GraphAPIError as error:
+            print 'GRAPH API ERROR CAUGHT!'
+            print '-'*80
+            pprint.pprint(error)
+            print '-'*80
+            self.oauth_exception_handler(error.result)
+            return None
+        except URLError as e:
+            self.url_exception_handler()
+            return None
+        except:
+            return None
+            
+        return comments
+    
+    def parse_comments(self, data):
+        comments = []
+        users = []
+        
+        for _set in data:
+            if _set['name'] == 'comments':
+                comments = _set['fql_result_set']
+            else:
+                users = _set['fql_result_set']
+        
+        for comment in comments:
+            for user in users:
+                if comment['fromid'] == user['uid']:
+                    comment['from'] = user
+                    break
+#        print 'In parse_comments...'
+#        print '-'*80
+#        pprint.pprint(comments)
+#        print '-'*80
+        
+        return comments
+    
+    def generate_comments(self, comments, post_id):
+        params = [{"comments":comments},
+                  {"post_id":post_id}]
+        page = Template(file = '/usr/share/eos-social/templates/comments-blank.html', searchList = params)
+        return page
+    
+    
         
 def main():
     try:
